81816ae7bd7e803f7b21067c9e7a333f6b024d54
Koialle <yoite.hikari@gmail.com>
2017-11-29 14:59:03 +0100

    Broadcast and notification

--- app/src/main/java/com/example/epulapp/quizzandroid/AbstractRootActivity.java
+++ app/src/main/java/com/example/epulapp/quizzandroid/AbstractRootActivity.java
@@ -3,15 +3,23 @@ package com.example.epulapp.quizzandroid;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-
-import static com.example.epulapp.quizzandroid.R.id.button2;
 
 public class AbstractRootActivity extends AppCompatActivity implements MenuFragment.MenuCallback {
 
+    private BroadcastReceiver receiver;
     private FragmentManager fragmentManager;
 
     @Override
@@ -19,12 +27,56 @@ public class AbstractRootActivity extends AppCompatActivity implements MenuFragm
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_quizz);
 
+        receiver = new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.d("notification", "Received broadcast");
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.notification_icon)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+
+                Intent result = new Intent(context, AbstractRootActivity.class);
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+                stackBuilder.addParentStack(AbstractRootActivity.class);
+                stackBuilder.addNextIntent(result);
+
+                PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+                builder.setContentIntent(pendingIntent);
+                NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                manager.notify(1, builder.build());
+            }
+        };
+
         fragmentManager = getFragmentManager();
         MenuFragment menu = new MenuFragment();
         changeFragment(menu, false);
     }
 
-    public void changeFragment(Fragment frag, boolean isReplace){
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Register receiver
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, new IntentFilter("notification"));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Unregister receiver
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
+    }
+
+    @Override
+    public void onButtonClicked(View view) {
+        QuizzFragment question = new QuizzFragment();
+        changeFragment(question, true);
+    }
+
+    private void changeFragment(Fragment frag, boolean isReplace){
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         if(isReplace){
             fragmentTransaction.replace(R.id.fragment_container, frag);
@@ -33,11 +85,5 @@ public class AbstractRootActivity extends AppCompatActivity implements MenuFragm
         }
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
-    }
-
-    @Override
-    public void onButtonClicked(View view) {
-        QuizzFragment question = new QuizzFragment();
-        changeFragment(question, true);
     }
 }
--- app/src/main/java/com/example/epulapp/quizzandroid/menuFragment.java
+++ app/src/main/java/com/example/epulapp/quizzandroid/menuFragment.java
@@ -11,10 +11,10 @@ import android.widget.Button;
 public class MenuFragment extends Fragment {
 
     public interface MenuCallback {
-        public void onButtonClicked(View view);
+        void onButtonClicked(View view);
     }
 
-    private MenuCallback activity;
+    private MenuCallback menuCallback;
 
     public MenuFragment() {
         // Required empty public constructor
@@ -24,12 +24,11 @@ public class MenuFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_menu, container, false);
 
-        Button button = view.findViewById(R.id.button);
+        Button button = (Button) view.findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //@TODO callback to activity
-                activity.onButtonClicked(view);
+            menuCallback.onButtonClicked(view);
             }
         });
 
@@ -40,7 +39,7 @@ public class MenuFragment extends Fragment {
     public void onAttach(Context context) {
         super.onAttach(context);
         try {
-            activity = (MenuCallback) context;
+            menuCallback = (MenuCallback) context;
         } catch (ClassCastException e) {
             throw new ClassCastException(String.valueOf(context) + " must implement onButtonClicked");
         }
